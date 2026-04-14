2140ac772b3c3016e3822e9a1990480c4efbcb0a
Mika <Mika.k.pearson@Gmail.com>
2026-04-14 23:56:44 +0200

    monaco color rules are broken -in their own system- shit like this is why i dont ever touch vscode

--- src/ts/html/components/input/colors.ts
+++ src/ts/html/components/input/colors.ts
@@ -1,4 +1,7 @@
 const COLOR_NAMES = ["blue", "green", "pink", "red", "yellow", "orange"];
 export const COLORS = COLOR_NAMES.reduce((acc, name) => ({
-  ...acc, [name]: getComputedStyle(document.documentElement).getPropertyValue(`--${name}`).trim()
+  ...acc, [name]: getComputedStyle(document.documentElement)
+    .getPropertyValue(`--${name}`)
+    .trim()
+    .replace('#', '')  // strip # for Monaco rules
 }), {} as Record<string, string>);
